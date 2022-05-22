04f3a0c9290eb7139e6e3e598233a0b0415b5aa7
Arpit <arpit27kr@gmail.com>
2022-05-21 22:40:08 -0700

    Add files via upload

---
+++ Basics/addingwithwarn.js
@@ -0,0 +1,12 @@
+var a=50;
+var b=60;
+var c=80;
+console.warn(a+b+c); 
+
+/* warn() function from console class of Node. js is used to display the warning messages on the console. 
+It prints to stderr with newline. Note: This function is an alias of console.*/
+
+console.log(a+b+c); //Using Console.log
+
+
+console.error(a+b+c); //Using Console.error
